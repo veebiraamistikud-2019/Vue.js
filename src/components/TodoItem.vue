86826b9b9f23dde2e230ee6cae66248d44713601
veliva <31840289+veliva@users.noreply.github.com>
2019-11-10 12:48:19 +0200

    done

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,47 @@
+<template>
+    <li>
+       <span v-bind:class="{done: todo.completed}">
+           <input type="checkbox" v-on:change="todo.completed = !todo.completed">
+           {{todo.title}}
+       </span>
+       <button class="btn-remove" v-on:click="$emit('remove-todo', todo.id)">X</button>
+    </li>
+</template>
+
+<script>
+export default {
+    props:{
+        todo: { 
+            type: Object,
+            required: true
+        },
+        index: Number
+    }
+}
+</script>
+
+<style scoped>
+    li {
+    transition: transform 1s;
+    display:flex;
+	margin:0px -3rem 18px;
+	padding:0.5rem 3rem;
+	justify-content:space-between;
+	align-items:center;
+    color: #838383;
+    background-color: #FFFFFF;
+    -webkit-box-shadow: 0px 11px 33px -9px rgba(0,0,0,0.75);
+    -moz-box-shadow: 0px 11px 33px -9px rgba(0,0,0,0.75);
+    box-shadow: 0px 11px 33px -9px rgba(0,0,0,0.75);
+    }
+    .done{
+        opacity: .6;
+        text-decoration: line-through;
+    }
+    input{
+      margin-right:1.5em;
+    }
+    .btn-remove {
+        background-color: #FF4B50;
+    }
+</style>
